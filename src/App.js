bef29ef1d5d030425facce05ed9a984fa4e9c6f0
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2021-12-01 13:42:41 +0100

    inlämning2 done!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Routing } from "./routes/Routing";
 import { Navigation } from "./component/Navigation"
+import { GlobalStyles } from "./shared/styles/GlobalStyles"
  
 export const App = () => {
   return (
     <div>
+      <GlobalStyles />
       <Routing>
         <Navigation />
       </Routing>
