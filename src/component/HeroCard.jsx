990b25f1d5c8bcc23668ab74ea3fb0b4719fa439
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2021-12-05 13:50:28 +0100

    added the right files

---
+++ src/component/HeroCard.jsx
@@ -0,0 +1,57 @@
+import styled from "styled-components"
+
+export const HeroCard = props => {
+    const img = `${props.character?.thumbnail?.path}.${props.character?.thumbnail?.extension}`
+    return (
+        <div>
+            <Card>
+                <HeroImg src={img} alt="" />                
+                    <CardInfoBox>
+                        <h2>{props.character?.name}</h2>
+                        <CardInfo>
+                            {props.character?.description}
+                        </CardInfo>
+                        <Copyright>{props.copyright} </Copyright>                        
+                    </CardInfoBox>
+            </Card>
+        </div>
+    )
+}
+
+const Card = styled.div`position: relative;
+    display: flex;
+    background: linear-gradient(
+        to left top,
+        rgba(255, 255, 255, 0.2),
+        rgba(255, 255, 255, 0.5)
+    );
+    width: 600px;
+    border-radius: 1rem;
+    margin: auto;
+    padding: 1rem;
+    box-shadow: 6px 6px 20px rgb(0, 0, 0);
+    justify-content: space-between; 
+`
+const HeroImg = styled.img`
+    width: 12rem;
+    height: 12rem;
+`
+const CardInfoBox = styled.div`    
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-left: 1em;
+    height: 10rem;
+`
+const CardInfo = styled.p`
+    color: antiquewhite;
+    font-size: 14px;
+`
+const Copyright = styled.p`
+text-align: center;
+color: #fbca03;
+text-shadow: 1px 1px #ff0000;
+position: absolute;
+bottom: 0;
+margin-bottom: 1rem;
+`
